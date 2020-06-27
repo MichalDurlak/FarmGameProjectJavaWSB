5975c5de4f95dcc3ad0d400029e95124607452c5
MichulolGitHub <durlak.m@wp.pl>
2020-06-27 19:50:10 +0200

    stage 3 - add options: 1,9 and 0 to menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -28,6 +28,18 @@ public class Main {
 
         while(playerChose!=0){
             switch(playerChose){
+                case 1:
+
+                    System.out.println("     ****************************************");
+                    System.out.println("Your balance: "+ player.getCash()+"$");
+                    System.out.println("Your farmland: "+ player.farmland+" hectare");
+                    System.out.println("Your animals: "+player.animals);
+
+                    System.out.println("Your buildings: "+player.buildings);
+                    System.out.println("Your seeds: "+player.seeds+" (max seeds: "+player.getTempSeeds()+", because you have "+player.buildings+" buildings.");
+                    System.out.println("     ****************************************");
+                    break;
+
                 case 9:
                     if (numberOfWeek >= 52){
                         numberOfWeek = 1;
@@ -36,16 +48,18 @@ public class Main {
                         numberOfWeek = numberOfWeek+1;
                     }
 
-                break;
-            }
+                    player.setTempSeeds();
+                    playerChose = menu(numberOfWeek,numberOfYear);
 
+            }
+            player.setTempSeeds();
             playerChose = menu(numberOfWeek,numberOfYear);
 
         }
 
         System.out.println("     ****************************************");
         System.out.println("\n Good bye "+ namePlayerScan +".");
-
+        System.out.println("     ****************************************");
     }
 
 
@@ -54,7 +68,7 @@ public class Main {
         System.out.println();
         System.out.println("Actual Date: "+numberOfWeek+" week. Year "+numberOfYear);
         System.out.println("*                 MENU                 *");
-        System.out.println("     1. ");
+        System.out.println("     1. Check stats.");
         System.out.println("     2. ");
         System.out.println("     3. ");
         System.out.println("     9. Next week.");
@@ -66,6 +80,8 @@ public class Main {
         return choose;
 
     }
+
+
 
 
 }
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,5 +1,4 @@
 package com.company;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Player {
@@ -7,6 +6,7 @@ public class Player {
     String name;
     int farmland,buildings, animals,seeds;
     private int cash;
+    private int tempSeeds;
 
     public Player(String name){
         this.name = name; // wprowadzenie imienia gracza
@@ -15,12 +15,14 @@ public class Player {
         int rand = random.nextInt(1000,5000);
 
         this.cash = rand;
-
-
         this.farmland = 0;
-        this.buildings = 0;
         this.animals = 0;
+        this.buildings = 0;
         this.seeds = 0;
+        this.tempSeeds = 0;
+
+
+
     }
 
 
@@ -28,8 +30,33 @@ public int getCash(){
         return cash;
 }
 
+public void setTempSeeds(){
+    if (buildings == 0){
+        this.tempSeeds = 0;
+    } else if (buildings == 1){
+        this.tempSeeds = 10*buildings;
+    } else if (buildings == 2){
+        this.tempSeeds = 10*buildings;
+    } else if (buildings == 3){
+        this.tempSeeds = 10*buildings;
+    } else if (buildings == 4){
+        this.tempSeeds = 10*buildings;
+    } else if (buildings == 5){
+        this.tempSeeds = 10*buildings;
+    } else if (buildings == 6){
+        this.tempSeeds = 10*buildings;
+    } else if (buildings == 7){
+        this.tempSeeds = 10*buildings;
+    } else if (buildings == 8){
+        this.tempSeeds = 10*buildings;
+    } else if (buildings == 9){
+        this.tempSeeds = 10*buildings;
+    } else if (buildings == 100){
+        this.tempSeeds = 10*buildings;
+    }
 
-
+}
+public int getTempSeeds() {return tempSeeds;}
 
 
 }
